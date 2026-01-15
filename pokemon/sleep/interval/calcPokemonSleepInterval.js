b7213ec79ec1ec410a4908f74acdff752544b0b6
Shota Hayashi <shota.hayashi.0101@gmail.com>
2026-01-16 00:04:46 +0900

    微修正

--- pokemon/sleep/interval/calcPokemonSleepInterval.js
+++ pokemon/sleep/interval/calcPokemonSleepInterval.js
@@ -69,13 +69,12 @@ async function selectPokemon(pokemons) {
 }
 
 (async function main() {
-  // --- TSV読み込み ---
   const pokemons = loadPokemons("./estimatedValueTable.tsv");
 
   // --- ポケモン選択 ---
   const pokemon = await selectPokemon(pokemons);
 
-  console.log(`\n選択されたポケモン: ${pokemon.name}`);
+  console.log(`選択されたポケモン: ${pokemon.name}`);
 
   const 性格選択 = readlineSync.keyInSelect(
     ["無し","上昇", "下降"],
@@ -102,22 +101,19 @@ async function selectPokemon(pokemons) {
   console.log(`食材確率（ベース）: ${(pokemon.食材確率 * 100).toFixed(2)}%`);
   console.log(`食材確率（補正後）: ${(食材確率 * 100).toFixed(2)}%`);
 
-  // --- 数値入力 ---
   const 最大所持数 = readlineSync.questionFloat("最大所持数を入力してください: ");
   const 速度 = readlineSync.questionFloat("速度を入力してください: ");
   const 食材の個数 = readlineSync.questionFloat("食材の個数を入力してください: ");
   const きのみの個数 = readlineSync.questionFloat("きのみの個数を入力してください: ");
 
-  // --- 計算 ---
   const 一回あたりの個数 =
     食材確率 * 食材の個数 + (1 - 食材確率) * きのみの個数;
+  console.log(`一回あたりの個数: ${(一回あたりの個数).toFixed(2)}`);
 
   const 分 = (最大所持数 / 一回あたりの個数) * (速度 / 2.222);
   const 時間 = Math.floor(分 / 60);
   const 分残り = 分 % 60;
 
-  // --- 出力 ---
-  console.log("\n=== 結果 ===");
   console.log(`分（合計）: ${分.toFixed(2)} 分`);
   console.log(`時間換算: ${時間} 時間 ${分残り.toFixed(2)} 分`);
 })();
