4634516309d16934afa5d237b1a442a2ced54e11
Shota Hayashi <shota.hayashi.0101@gmail.com>
2026-01-17 00:50:38 +0900

    元気回復ボーナスの考慮

--- pokemon/sleep/recovery/calcRecoveryAmount.bash
+++ pokemon/sleep/recovery/calcRecoveryAmount.bash
@@ -10,13 +10,12 @@ max=$((8 * 60 + 30))
 
 # 回復量（小数点以下切り捨て）
 recovery=$((100 * time / max))
+recovery_bonus=$((recovery * 114 / 100)) # 元気回復ボーナスを考慮
+recovery_nature_downward=$((recovery * 88 / 100)) # 性格補正後（0.88倍 → 88/100 で整数演算）
+recovery_nature_downward_bonus=$((recovery_nature_downward * 114 / 100)) # 性格補正後（0.88倍 → 88/100 で整数演算）
 
-# 性格補正後（0.88倍 → 88/100 で整数演算）
-recovery_nature_downward=$((recovery * 88 / 100))
-
-# 出力
-echo "回復量：$recovery"
-echo "回復量：$recovery_nature_downward （性格下降補正）"
-echo "元気が $((100 - recovery)) なら全回復"
-echo "元気が $((100 - recovery_nature_downward)) なら性格下降補正でも全回復"
+echo "$((100 - recovery)) 補正なしで全回復"
+echo "$((100 - recovery_bonus)) 元気回復ボーナス"
+echo "$((100 - recovery_nature_downward)) 性格下降補正"
+echo "$((100 - recovery_nature_downward_bonus)) 性格下降補正と元気回復ボーナス"
 
