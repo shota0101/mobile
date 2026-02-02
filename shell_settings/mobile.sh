165027cb5ad727724604781121e49343ae4f55a0
Shota Hayashi <shota.hayashi.0101@gmail.com>
2026-02-02 19:26:01 +0900

    Pokemon Sleep関連コマンドを追加

--- shell_settings/mobile.sh
+++ shell_settings/mobile.sh
@@ -3,3 +3,24 @@ function ca() {
     directory=${directory/\~/$HOME} # チルダをホームディレクトリに置換
     cd "${directory}"
 }
+
+alias upsh="source ~/.zshrc"
+
+DAEMON_PATH=~/Documents/git/mobile/pokemon/sleep/daemon
+
+function d() {
+    cd $DAEMON_PATH
+    bash daemon.sh
+}
+
+function m() {
+    cd $DAEMON_PATH
+    bash mark.sh
+    bash state.sh
+}
+
+function m() {
+    cd $DAEMON_PATH
+    bash update.sh
+}
+
