a1315c1fd130d541d77a119b1a7ebe042f9469d3
Shota Hayashi <shota.hayashi.0101@gmail.com>
2026-02-07 13:44:38 +0900

    shell整理

--- shell_settings/mobile.sh
+++ shell_settings/mobile.sh
@@ -7,11 +7,12 @@ function ca() {
 alias upsh="source ~/.zshrc"
 
 DAEMON_PATH=~/Documents/git/mobile/pokemon/sleep/daemon
+TCG_PATH=~/Documents/git/mobile/pokemon/sleep/daemon
 TODO_PATH=~/Documents/git/todo
 
 alias d="bash ${DAEMON_PATH}/daemon.sh"
-alias s="bash ${DAEMON_PATH}/mark.sh ; bash ${DAEMON_PATH}/state.sh"
-alias p="bash ~/Documents/git/mobile/pokemon/tcg/12hourCheck.bash"
+alias s="bash ${DAEMON_PATH}/mark.sh ; bash ${DAEMON_PATH}/state.sh ; bash ${TCG_PATH}/listExpiredFiles.bash"
+alias 12="bash ${TCG_PATH}/12hourCheck.bash"
 
 function r() {
     cd $DAEMON_PATH
@@ -27,4 +28,7 @@ function m() {
     cd $TODO_PATH
     bash mark_due.sh
 }
+
+bash Documents/git/mobile/pokemon/sleep/daemon/state.sh
+bash Documents/git/mobile/pokemon/tcg/listExpiredFiles.bash
 
