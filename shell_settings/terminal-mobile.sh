753d2d14ae47e4a2fbc5f2417a9da219374de809
Shota Hayashi <shota.hayashi.0101@gmail.com>
2026-03-26 22:49:13 +0900

    add git command

--- shell_settings/terminal-mobile.sh
+++ shell_settings/terminal-mobile.sh
@@ -1,4 +1,8 @@
 termux-clipboard-set
 termux-clipboard-get
 termux-notification -t "title" -c "message"
+git diff --no-index file1.txt file2.txt
+git commit --allow-empty -m "first commit"
+git config --global core.quotepath false
 git config --global user.name "Shota Hayashi" ; git config --global user.email "shota.hayashi.0101@gmail.com"
+ssh-keygen -t rsa
