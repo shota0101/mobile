8df73275c789900c82d440b918ac418eb9441d2a
Shota Hayashi <shota.hayashi.0101@gmail.com>
2026-03-27 15:34:46 +0900

    初期化処理に会計簿スクリプトを追加

--- shell_settings/mobile.sh
+++ shell_settings/mobile.sh
@@ -5,35 +5,22 @@ function j() {
 }
 
 LOCAL_COMMAND_LIST=~/Documents/git/mobile/shell_settings/terminal-mobile.sh
-t() {
+tt() {
     cat \
 	$LOCAL_COMMAND_LIST \
 	| fzf | tr -d '\n' | pbcopy
 }
 
+alias m="bash ~/Documents/git/todo/markAsDone.sh"
+
 DAEMON_PATH=~/Documents/git/mobile/pokemon/sleep/daemon
-TCG_PATH=~/Documents/git/mobile/pokemon/tcg
-TODO_PATH=~/Documents/git/todo
-
-alias d="bash ${DAEMON_PATH}/daemon.sh"
-alias s="bash ${DAEMON_PATH}/mark.sh ; bash ${DAEMON_PATH}/state.sh ; bash ${TCG_PATH}/listExpiredFiles.bash"
-alias 12="bash ${TCG_PATH}/12hourCheck.bash"
-
 function r() {
     cd $DAEMON_PATH
     bash reflect.sh
 }
 
-function o() {
-    cd $TODO_PATH
-    bash list.sh
+function h() {
+    cd ~/Documents/git/household-accounts/
+    bash run.sh
 }
-
-function m() {
-    cd $TODO_PATH
-    bash mark_due.sh
-}
-
-bash Documents/git/mobile/pokemon/sleep/daemon/state.sh
-bash Documents/git/mobile/pokemon/tcg/listExpiredFiles.bash
 
