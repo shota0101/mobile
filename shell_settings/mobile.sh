d80addb0e630f6ad8a291b60f8bd9fe1b43c8cdf
Shota Hayashi <shota.hayashi.0101@gmail.com>
2026-05-05 07:42:30 +0900

    ヘルプのインデントを揃える

--- shell_settings/mobile.sh
+++ shell_settings/mobile.sh
@@ -39,27 +39,27 @@ print_wifi_status() {
     echo '❌Wi-Fi接続確認できませんでした'
 }
 
-echo "media : organize media"
-alias media="bash ~/Documents/git/mobile/organizeMedia/organizeAndroidMediaFiles.sh"
-
-echo "gall : git pull all"
-alias gall="bash ~/Documents/git/company/script/pull-all.sh"
-
 echo "p : Pokémon Sleep"
 function p() {
     cd ~/Documents/git/mobile/pokemon/sleep/daemon
     bash reflect.sh
 }
 
-echo "lt : log temperature"
-alias lt="bash ~/Documents/git/temperature/log.bash"
-echo "ss : sleep suggest"
-alias ss="bash ~/Documents/git/temperature/search.bash"
-
-echo "r : rotation"
+echo "r\t: rotation"
 alias r="bash ~/Documents/git/rotation/rotation.sh"
 
-echo "m : mark tasks as done"
+echo "lt\t: log temperature"
+alias lt="bash ~/Documents/git/temperature/log.bash"
+echo "ss\t: sleep suggest"
+alias ss="bash ~/Documents/git/temperature/search.bash"
+
+echo "gpa\t: git pull all"
+alias gall="bash ~/Documents/git/company/script/pull-all.sh"
+
+echo "om\t: organize media"
+alias media="bash ~/Documents/git/mobile/organizeMedia/organizeAndroidMediaFiles.sh"
+
+echo "m\t: mark tasks as done"
 alias m="bash ~/Documents/git/todo/markAsDone.sh"
 bash ~/Documents/git/todo/listUnfinishedTasks.sh
 
